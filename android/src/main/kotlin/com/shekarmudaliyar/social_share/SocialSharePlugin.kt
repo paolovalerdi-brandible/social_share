270bd9283209b5c69b735f51c4a6c687694ccefd
Paolo Valerdi <99295888+paolovalerdi-brandible@users.noreply.github.com>
2025-07-10 15:57:09 -0600

    Update SocialSharePlugin.kt

--- android/src/main/kotlin/com/shekarmudaliyar/social_share/SocialSharePlugin.kt
+++ android/src/main/kotlin/com/shekarmudaliyar/social_share/SocialSharePlugin.kt
@@ -16,7 +16,7 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
+// import io.flutter.plugin.common.PluginRegistry.Registrar
 import java.io.File
 import java.net.URLEncoder
 import android.content.ClipData
